86be2fe98d608d99b989a461e2cf8de04ecf5407
Emilien <emilien.paris@etudiant.univ-lr.fr>
2021-12-06 16:25:23 +0100

    :bug: : correcting lint

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,4 @@
 from fastapi.testclient import TestClient
-from starlette import responses
-
 from .app import app
 
 client = TestClient(app)
@@ -16,6 +14,6 @@ def test_generation_token():
 
 def test_validation_token():
     response = client.get(
-        '/client/scanner/validation?user=ZENvZGU=&scanner=1&date=2021340143735&treatmentId=ZENvZGUxMjAyMTM0MDE0MzczNQ==')
+        '/client/scanner/validation?user=ZENvZGU=&scanner=1&date=2021340143735&treatmentId=ZENvZGUxMjAyMTM0MDE0MzczNQ=='
+    )
     assert response.json() == {"status":200, "request":"ZENvZGUxMjAyMTM0MDE0MzczNQ==", "result": bool(1)}
-
